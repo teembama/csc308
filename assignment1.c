e1f5386337bf1d1b0f6d07d3553336aa6f1ec611
root <root@THERESE-PC.localdomain>
2024-11-11 21:50:28 +0100

    second commit

---
+++ assignment1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+    int age; 
+    int experience;
+    int salary;
+    printf("Enter your age:");
+    scanf("%d", &age);
+
+    printf("Are you experienced?, Enter 1 if you are experienced and 0 if you are not experienced");
+    scanf("%d", &experience);
+
+    if (experience == 1 & age >= 40){
+        salary = 560000;
+        printf("Your salary is: N%d\n", salary);
+    }
+    else if (experience == 1 & age < 40 & age >= 30){
+        salary = 480000;
+        printf("Your salary is: N%d\n", salary);
+    }
+    else if (experience == 1 & age < 28){
+        salary = 300000;
+        printf("Your salary is: N%d\n", salary);
+    }
+    else if (experience == 0){
+        salary = 100000;
+        printf("Your salary is: N%d\n", salary);
+    }
+    else{
+        printf("This is an invalid situation");
+    }
+}
